8e1fded33bbe17cc9dd637216871a2c48d552d2b
Yeri <arifyeripratama@gmail.com>
2019-11-18 21:29:57 +0700

    update rofi config

--- setup/arch/essentials.sh
+++ setup/arch/essentials.sh
@@ -2,4 +2,4 @@
 
 # essentials packages for arch linux
 
-sudo -S pacman --noconfirm -Syu openssh zsh neovim tmux fzf ripgrep postgresql php php-pgsql composer mariadb nodejs npm ruby feh network-manager-applet volumeicon picom scrot thunar python-pynvim xclip imagemagick zip unzip
+sudo -S pacman --noconfirm -Syu openssh zsh neovim tmux fzf ripgrep postgresql php php-pgsql composer mariadb nodejs npm ruby feh network-manager-applet volumeicon picom scrot thunar python-pynvim xclip imagemagick zip unzip xdg-user-dirs
