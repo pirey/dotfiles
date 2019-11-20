3914e52ea9c388e010037190587ce8c6032c8035
Yeri <arifyeripratama@gmail.com>
2019-11-20 11:35:51 +0700

    polybar bottom  setup

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -6,5 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar main >>/tmp/polybar1.log 2>&1 &
+# Launch top and bottom bar
+polybar top >>/tmp/polybar-top.log 2>&1 &
+echo $! >/tmp/polybar-top.pid
+
+polybar bottom >>/tmp/polybar-bottom.log 2>&1 &
+echo $! >/tmp/polybar-bottom.pid
