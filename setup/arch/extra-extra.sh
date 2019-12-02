d75ce74d3c8cd3b37e3cf6099855ad189af5042d
Yeri <arifyeripratama@gmail.com>
2019-12-02 14:46:23 +0700

    add lxml

--- setup/arch/extra-extra.sh
+++ setup/arch/extra-extra.sh
@@ -2,4 +2,4 @@
 
 # extra packages for arch linux
 
-sudo -S pacman --noconfirm -Syu libreoffice-still gimp gimp-help-en inkscape
+sudo -S pacman --noconfirm -Syu libreoffice-still gimp gimp-help-en inkscape python-lxml python2-lxml
