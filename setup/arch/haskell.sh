ffd440155c708182b6e141f907bbecf85889a48f
Yeri <arifyeripratama@gmail.com>
2019-12-05 15:32:00 +0700

    set ghc

--- setup/arch/haskell.sh
+++ setup/arch/haskell.sh
@@ -5,6 +5,7 @@
 ( mkdir -p ~/.ghcup/bin && curl https://gitlab.haskell.org/haskell/ghcup/raw/master/ghcup > ~/.ghcup/bin/ghcup && chmod +x ~/.ghcup/bin/ghcup) && echo "Success"
 
 ghcup install
+ghcup set
 ghcup install-cabal
 
 cabal new-install cabal-install
