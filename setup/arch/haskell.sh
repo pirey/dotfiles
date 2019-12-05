cb26a878060f0580fa2e738f752f482b88294501
Yeri <arifyeripratama@gmail.com>
2019-12-05 14:14:59 +0700

    update ghcup setup

--- setup/arch/haskell.sh
+++ setup/arch/haskell.sh
@@ -4,4 +4,7 @@
 
 ( mkdir -p ~/.ghcup/bin && curl https://gitlab.haskell.org/haskell/ghcup/raw/master/ghcup > ~/.ghcup/bin/ghcup && chmod +x ~/.ghcup/bin/ghcup) && echo "Success"
 
-# sudo -S pacman --noconfirm -Syu
+ghcup install
+ghcup install-cabal
+
+cabal new-install cabal-install
