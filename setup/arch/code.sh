a4cbb2469f3056a1ba365376a308a02ac981f43d
Yeri <arifyeripratama@gmail.com>
2019-12-10 11:01:57 +0700

    setup code desktop entry

---
+++ setup/arch/code.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+######################################
+# vscode
+######################################
+
+currentscript=$(realpath $0)
+archdir=$(dirname $currentscript)
+setupdir=$(dirname $archdir)
+desktopdir=~/.local/share/applications
+
+mkdir -p $desktopdir
+
+rm /tmp/code.desktop
+
+echo "[Desktop Entry]" >> /tmp/code.desktop
+echo "Version=1.0" >> /tmp/code.desktop
+echo "Type=Application" >> /tmp/code.desktop
+echo "Name=Visual Studio Code" >> /tmp/code.desktop
+echo "Icon=${HOME}/dotfiles/icons/code.png" >> /tmp/code.desktop
+echo "Exec=code" >> /tmp/code.desktop
+echo "Comment=Develop with pleasure!" >> /tmp/code.desktop
+echo "Categories=Development;IDE;" >> /tmp/code.desktop
+echo "Terminal=false" >> /tmp/code.desktop
+
+desktop-file-install --dir=$HOME/.local/share/applications /tmp/code.desktop
+update-desktop-database $HOME/.local/share/applications
