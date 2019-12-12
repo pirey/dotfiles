09cc7937ea3b73e8760aa2cd00d821dbb388bb21
Yeri <arifyeripratama@gmail.com>
2019-12-12 13:49:30 +0700

    add usbutils

--- setup/arch/essentials.sh
+++ setup/arch/essentials.sh
@@ -2,6 +2,6 @@
 
 # essentials packages for arch linux
 
-sudo -S pacman --noconfirm -Syu openssh zsh neovim tmux fzf ripgrep feh network-manager-applet volumeicon picom scrot thunar xclip imagemagick p7zip zip unzip xdg-user-dirs xf86-input-libinput xorg xorg-xinit xorg-xinput xorg-xbacklight xautolock i3 rofi alacritty kitty pulseaudio pavucontrol udisks2 udiskie
+sudo -S pacman --noconfirm -Syu openssh zsh neovim tmux fzf ripgrep feh network-manager-applet volumeicon picom scrot thunar xclip imagemagick p7zip zip unzip xdg-user-dirs xf86-input-libinput xorg xorg-xinit xorg-xinput xorg-xbacklight xautolock i3 rofi alacritty kitty pulseaudio pavucontrol udisks2 udiskie usbutils
 
 # if you want to use Gnome or other DE, add it accordingly
