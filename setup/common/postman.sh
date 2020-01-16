8f04e6337555d2cd0d966554531b9e1653d6f63a
Yeri <arifyeripratama@gmail.com>
2020-01-16 16:40:13 +0700

    postman icon

--- setup/common/postman.sh
+++ setup/common/postman.sh
@@ -22,12 +22,18 @@ rm -rf $installdir/*
 mv /tmp/Postman/* $installdir
 cd -
 
-cat << 'POSTMAN' > /tmp/postman.desktop
-[Desktop Entry]
-Name=Postman
-Type=Application
-Exec=Postman
-POSTMAN
+
+# launcher
+if [ -f /tmp/postman.desktop ]; then
+    rm /tmp/postman.desktop
+fi
+
+echo "[Desktop Entry]" >> /tmp/postman.desktop
+echo "Name=Postman" >> /tmp/postman.desktop
+echo "Type=Application" >> /tmp/postman.desktop
+echo "Exec=Postman" >> /tmp/postman.desktop
+echo "Icon=${HOME}/dotfiles/icons/postman.png" >> /tmp/postman.desktop
+echo "Terminal=false" >> /tmp/postman.desktop
 
 desktop-file-install --dir=$HOME/.local/share/applications /tmp/postman.desktop
 update-desktop-database $HOME/.local/share/applications
