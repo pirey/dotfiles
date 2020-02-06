28a2c037f160e9cea1117a8a8960ba756732f54f
Yeri <arifyeripratama@gmail.com>
2020-02-06 19:57:06 +0700

    add note to install haskell ide engine

--- setup/arch/dev-tools/haskell.sh
+++ setup/arch/dev-tools/haskell.sh
@@ -7,3 +7,8 @@ echo "Installing devtools packages..."
 echo "======================================"
 
 sudo -S pacman --noconfirm -Syu ghc ghc-libs ghc-static cabal-install stack
+
+# git clone https://aur.archlinux.org/haskell-ide-engine.git /tmp/hie
+# cd /tmp/hie
+# makepkg -si
+# cd -
