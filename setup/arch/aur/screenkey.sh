5577a277d2c7dd4bfe3a06568c15adff1f50879f
Yeri <arifyeripratama@gmail.com>
2020-02-26 21:27:52 +0700

    update screenkey installation script

--- setup/arch/aur/screenkey.sh
+++ setup/arch/aur/screenkey.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# install dependency manually
+git clone https://aur.archlinux.org/python2-distutils-extra.git /tmp/python2-distutils-extra
 git clone https://aur.archlinux.org/screenkey.git /tmp/screenkey-aur
+
+cd /tmp/python2-distutils-extra
+makepkg -si --noconfirm
+
 cd /tmp/polybar-aur
 makepkg -si --noconfirm
 cd -
