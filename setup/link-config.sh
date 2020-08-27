d9191daaaff5a397e730914df91cd3906fca80be
Yeri <arifyeripratama@gmail.com>
2020-08-27 09:46:15 +0700

    move ncmpcpp config

--- setup/link-config.sh
+++ setup/link-config.sh
@@ -27,7 +27,6 @@ link_home .editorconfig
 link_home .npmrc
 link_home .prettierrc
 link_home .xinitrc
-link_home .ncmpcpp
 link_home .spacemacs
 link_home .railsrc
 link_home .opam-helper
@@ -46,12 +45,11 @@ link_file $DOTFILES/home/.oh-my-zsh/custom/themes/pirey.zsh-theme $HOME/.oh-my-z
 # .config
 link_xdg_config Dharkael
 link_xdg_config nvim
-link_xdg_config alacritty
 link_xdg_config kitty
 link_xdg_config polybar
 link_xdg_config mpd
+link_xdg_config ncmpcpp
 link_xdg_config i3
-link_xdg_config i3status
 link_xdg_config rofi
 link_xdg_config picom
 link_xdg_config dunst
