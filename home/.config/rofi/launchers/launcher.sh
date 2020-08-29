95f7ab5a68dbb5f46884de5ccd4a648392b043a7
Yeri <arifyeripratama@gmail.com>
2020-08-29 11:27:11 +0700

    rofi modi for app launcher

--- home/.config/rofi/launchers/launcher.sh
+++ home/.config/rofi/launchers/launcher.sh
@@ -20,4 +20,4 @@ style="style_icons_full"
 # style="style_normal_grid"
 # style="style_normal_grid_full"
 
-rofi -no-lazy-grab -show drun -theme launchers/"$style".rasi
+rofi -no-lazy-grab -modi 'drun' -show drun -theme launchers/"$style".rasi
