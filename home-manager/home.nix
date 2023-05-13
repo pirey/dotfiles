2ebc84e7ae81409fef0ef274bbf3246244542d85
Yeri <arifyeripratama@gmail.com>
2023-05-13 18:02:50 +0700

    setup git

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -58,12 +58,73 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      core = {
+        editor = "nvim -u NONE";
+      };
+      pretty = {
+        custom = "%Cred%h%Creset - %C(bold blue)<%an>%C(yellow)%d%Creset %s %Cgreen(%cr) %Creset";
+      };
+    };
+    includes = [
+      {
+        path = "~/.gitconfig.local";
+      }
+    ];
+    aliases = {
+      l = "log --pretty=custom --abbrev-commit";
+      la = "log --branches --remotes --tags --oneline --decorate";
+      gap = "log --left-right --oneline --decorate";
+      compare = "log --left-right --oneline --decorate";
+      co = "checkout";
+      # list-files = show --format=\"\" --name-status
+      list-files = "diff-tree --no-commit-id --name-status -r";
+      # show all branch order by last commit
+      br = "for-each-ref --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'";
+      stash = "stash -u";
+      rlog = "reflog --date=local";
+      logno = "log --no-merges";
+      confg = "config --global --list";
+      committer = "shortlog -s -n --all";
+
+      # Find commits by code.
+
+      fc = "!f() { \
+          git log --pretty=custom --decorate --date=short -S\"$1\"; \
+      }; f";
+
+      # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+      # Find commits by commit message.
+
+      fm = "!f() { \
+          git log --pretty=custom --decorate --date=short --grep=\"$1\"; \
+      }; f";
+    };
+    ignores = [
+      "*.keystore"
+
+      "*~"
+      ".DS_Store"
+
+      ".eslintcache"
+      "node_modules/*"
+      ".vscode"
+      "*.swp"
+
+      # python
+      ".venv"
+    ];
+  };
 
   programs.fish = {
     enable = true;
     shellAbbrs = {
       l = "ls -alh";
+      z = "zoxide";
 
       ".." = "cd ..";
       "cd.." = "cd ..";
@@ -113,6 +174,11 @@
   };
 
   programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.zoxide = {
     enable = true;
     enableFishIntegration = true;
   };
