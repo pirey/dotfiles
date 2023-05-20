6aaa8b9ca9ec7f6bcc269cb8f8e185318d59ad1f
Yeri <arifyeripratama@gmail.com>
2023-05-20 21:31:30 +0700

    use home-manager master

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
