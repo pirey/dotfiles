481852e38b69b565f7cc13e3c0f801ea95d235f1
Yeri <arifyeripratama@gmail.com>
2023-11-12 03:41:55 +0700

    chore: update font selection

--- fedora/font/setup.sh
+++ fedora/font/setup.sh
@@ -3,9 +3,10 @@
 if ! fc-list | grep -i -q "JetBrainsMono"; then
   echo "Installing font..."
   wget -P /tmp https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+  wget -P /tmp https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/IntelOneMono.zip
   unzip -d ~/.local/share/fonts/JetBrainsMono /tmp/JetBrainsMono.zip
+  unzip -d ~/.local/share/fonts/IntelOneMono /tmp/IntelOneMono.zip
   fc-cache
 fi
 
 echo "Font installed"
-
