56030efbfa5fcba7b3b30a3151aa1779c9ed4593
Yeri <mail@yeripratama.com>
2026-02-27 04:48:27 +0700

    core: update packages

--- lua/specs.lua
+++ lua/specs.lua
@@ -19,12 +19,6 @@ local fugitive = {
   end,
 }
 local abolish = { src = "tpope/vim-abolish" }
-local autotag = {
-  src = "windwp/nvim-ts-autotag",
-  config = function()
-    require("nvim-ts-autotag").setup()
-  end,
-}
 local mason = {
   src = "mason-org/mason.nvim",
   config = function()
@@ -694,7 +688,6 @@ return {
   surround,
   fugitive,
   abolish,
-  autotag,
   treesj,
   conform,
   nvim_lint,
