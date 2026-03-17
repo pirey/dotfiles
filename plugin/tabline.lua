67b9555b6587f8cf373a3f6f0c82e03ee3c6de4e
Yeri <mail@yeripratama.com>
2026-03-17 19:29:49 +0700

    feat: update tabline

--- plugin/tabline.lua
+++ plugin/tabline.lua
@@ -1,51 +1,27 @@
-local function shorten_dir(dir)
-  local home = vim.env.HOME
-  return dir == "" and "" or vim.startswith(dir, home) and "~" .. dir:sub(#home + 1) or dir
-end
-
 local function render()
-  local tabs = {}
-  local by_name = {}
+  local line = ""
+  local cur = vim.api.nvim_tabpage_get_number(vim.api.nvim_get_current_tabpage())
+
   for i, tabpage in ipairs(vim.api.nvim_list_tabpages()) do
     local bufnr = vim.api.nvim_win_get_buf(vim.api.nvim_tabpage_get_win(tabpage))
-    local buf_name = vim.api.nvim_buf_get_name(bufnr)
-    local name = buf_name == "" and "[No Name]" or vim.fn.fnamemodify(buf_name, ":t")
-    local dir = buf_name == "" and "" or shorten_dir(vim.fn.fnamemodify(buf_name, ":h"))
-    tabs[i] = { name = name, dir = dir, modified = vim.bo[bufnr].modified, page = i }
-  end
-
-  for _, tab in ipairs(tabs) do
-    by_name[tab.name] = (by_name[tab.name] or 0) + 1
-  end
-
-  for _, tab in ipairs(tabs) do
-    if by_name[tab.name] > 1 then
-      tab.name = tab.dir .. "/" .. tab.name
-    end
-    tab.name = " " .. tab.name .. (tab.modified and " ●" or "") .. " "
-  end
-
-  local cur = vim.api.nvim_tabpage_get_number(vim.api.nvim_get_current_tabpage())
-  local cols = vim.go.columns
-  local line, shown = "", 0
-
-  for _, tab in ipairs(tabs) do
-    local w = vim.fn.strdisplaywidth(tab.name)
-    if shown + w <= cols then
-      line = line .. "%#" .. (tab.page == cur and "TabLineSel" or "TabLine") .. "#%" .. tab.page .. "T" .. tab.name .. "%T"
-      shown = shown + w
+    local name
+    if bufnr == 0 or not vim.api.nvim_buf_is_valid(bufnr) then
+      name = " [invalid] "
     else
-      break
+      local buf_name = vim.api.nvim_buf_get_name(bufnr)
+      name = buf_name == "" and "[No Name]" or (vim.fn.fnamemodify(buf_name, ":t") or "[No Name]")
+      local wins = #vim.api.nvim_tabpage_list_wins(tabpage)
+      local win_indicator = wins > 1 and " " .. wins .. " " or ""
+      name = " " .. name .. (vim.bo[bufnr].modified and " ●" or "") .. win_indicator .. " "
     end
+    line = line .. "%#" .. (i == cur and "TabLineSel" or "TabLine") .. "#%" .. i .. "T" .. name .. "%T"
   end
 
-  return line .. (shown <= cols - 4 and "%#TabLineFill#" or "%#TabLine# … ")
+  return line .. "%#TabLine#"
 end
 
 vim.go.tabline = render()
 
 vim.api.nvim_create_autocmd({ "TabEnter", "TabLeave", "BufEnter", "TabNew", "TabClosed" }, {
-  callback = function()
-    vim.go.tabline = render()
-  end
+  callback = function() vim.go.tabline = render() end
 })
