f979fe4e62b37e8d82cb92169254265df14765d9
Yeri <mail@yeripratama.com>
2026-03-31 17:55:44 +0700

    feat: ui2

--- init.lua
+++ init.lua
@@ -1,13 +1,11 @@
 vim.g.mapleader = " "
 
--- essentials
 vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.gdefault = true
 
--- nice to have
 vim.opt.splitright = true
 vim.opt.number = true
 vim.opt.signcolumn = "yes"
@@ -68,3 +66,27 @@ vim.cmd("autocmd QuickFixCmdPost grep,grep! copen")
 
 require("pack").setup(require("specs"))
 vim.cmd.colorscheme("nightfox")
+
+require("vim._core.ui2").enable({
+  enable = true, -- Whether to enable or disable the UI.
+  msg = { -- Options related to the message module.
+    ---@type 'cmd'|'msg' Default message target, either in the
+    ---cmdline or in a separate ephemeral message window.
+    ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+    ---or table mapping |ui-messages| kinds and triggers to a target.
+    targets = "cmd",
+    cmd = { -- Options related to messages in the cmdline window.
+      height = 0.5, -- Maximum height while expanded for messages beyond 'cmdheight'.
+    },
+    dialog = { -- Options related to dialog window.
+      height = 0.5, -- Maximum height.
+    },
+    msg = { -- Options related to msg window.
+      height = 0.5, -- Maximum height.
+      timeout = 4000, -- Time a message is visible in the message window.
+    },
+    pager = { -- Options related to message window.
+      height = 1, -- Maximum height.
+    },
+  },
+})
