a0eedbaf765f936e185255e857cf233778d41f6c
Yeri <mail@yeripratama.com>
2026-04-03 06:49:31 +0700

    feat: use onedark

--- init.lua
+++ init.lua
@@ -65,7 +65,7 @@ vim.cmd("autocmd WinEnter * if &buftype == 'terminal' | startinsert | endif")
 vim.cmd("autocmd QuickFixCmdPost grep,grep! copen")
 
 require("pack").setup(require("specs"))
-vim.cmd.colorscheme("kanagawa")
+vim.cmd.colorscheme("onedark")
 
 require("vim._core.ui2").enable({
   enable = true, -- Whether to enable or disable the UI.
