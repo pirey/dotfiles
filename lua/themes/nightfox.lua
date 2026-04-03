21de5548d9511a53fbbf92769b237dce756f685b
Yeri <mail@yeripratama.com>
2026-04-03 14:20:07 +0700

    fix: nightfox diff text highlight

--- lua/themes/nightfox.lua
+++ lua/themes/nightfox.lua
@@ -5,6 +5,7 @@ return {
       -- reduce yellow color usage, make it more blue-ish
       groups = {
         all = {
+          ["DiffText"] = { bg = "#2f5f73" },
           ["@tag.builtin.tsx"] = { link = "Keyword" },
           ["@type.tsx"] = { link = "Keyword" },
           ["@type.javascript"] = { link = "Keyword" },
