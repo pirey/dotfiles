c82867ab638781c39d23da176578089d0d037903
Yeri <mail@yeripratama.com>
2026-04-04 13:20:03 +0700

    feat: tabclose keymap

--- init.lua
+++ init.lua
@@ -45,7 +45,10 @@ vim.keymap.set("n", "<localleader>ts", "<cmd>below sp | lcd %:h | term<cr>", { s
 vim.keymap.set("n", "<localleader>t<tab>", "<cmd>tab sp | lcd %:h | term<cr>", { silent = true })
 
 -- tabpage
-vim.keymap.set("n", "<leader>q", "<cmd>tabclose<cr>", { silent = true })
+for _, key in ipairs({ "<leader>q", "<leader>x" }) do
+  vim.keymap.set("n", key, "<cmd>tabclose<cr>", { silent = true })
+end
+vim.keymap.set("n", "<leader>x", "<cmd>tabclose<cr>", { silent = true })
 vim.keymap.set("n", "<leader><tab>o", "<cmd>tabonly<cr>", { silent = true })
 vim.keymap.set("n", "<leader><tab>l", "<cmd>tabs<cr>", { silent = true })
 vim.keymap.set("n", "<leader><tab>n", "<cmd>tabnew<cr>", { silent = true })
