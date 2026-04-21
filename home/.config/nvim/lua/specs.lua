435b93c257d382b32aa23ad2383a235516eed8c7
Yeri <mail@yeripratama.com>
2026-04-21 16:26:14 +0700

    enable auto completion

--- home/.config/nvim/lua/specs.lua
+++ home/.config/nvim/lua/specs.lua
@@ -235,9 +235,14 @@ local lspconfig = {
     vim.api.nvim_create_autocmd("LspAttach", {
       group = augroup,
       callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client and client.server_capabilities.semanticTokensProvider then
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+        if client.server_capabilities.semanticTokensProvider then
           client.server_capabilities.semanticTokensProvider = nil
+        end
+
+        if client:supports_method('textDocument/completion') then
+          vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
         end
       end,
     })
