6980305c2c2935a18588a2734cff5daaec953a73
Yeri <mail@yeripratama.com>
2026-05-25 06:14:40 +0700

    emacs: separate GUI/TUI settings, add magit config, normalize indentation
    - Wrap GUI-only settings (frame, font, toolbar, scrollbar, line-spacing)
      in (display-graphic-p)
    - Wrap TUI-only settings (xterm-mouse-mode, pbcopy/pbpaste clipboard)
      in (unless (display-graphic-p))
    - Move menu-bar-mode -1 to apply universally
    - Add magit :config with diff-refine-hunk and log-arguments
    - Normalize indentation to consistent 4-space

--- home/.config/emacs/init.el
+++ home/.config/emacs/init.el
@@ -3,19 +3,13 @@
 
 ;;; Theme
 (add-to-list 'custom-theme-load-path
-             (expand-file-name "themes" user-emacs-directory))
+    (expand-file-name "themes" user-emacs-directory))
 (load-theme 'nvim-dark t)
 
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-(add-to-list 'default-frame-alist '(font . "Ioskeley Mono-15"))
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
 (fido-vertical-mode 1)
-(xterm-mouse-mode 1)
 (editorconfig-mode 1)
-(setq-default line-spacing 8
-    truncate-lines t)
+(menu-bar-mode -1)
+(setq-default truncate-lines t)
 (setq inhibit-startup-screen t
     mouse-wheel-flip-direction t
     mouse-wheel-tilt-scroll t
@@ -26,14 +20,23 @@
     auto-save-default nil
     make-backup-files nil)
 
-;; Clipboard in TUI
+;; GUI only
+(when (display-graphic-p)
+    (add-to-list 'default-frame-alist '(fullscreen . maximized))
+    (add-to-list 'default-frame-alist '(font . "Ioskeley Mono-15"))
+    (tool-bar-mode -1)
+    (scroll-bar-mode -1)
+    (setq-default line-spacing 8))
+
+;; TUI only
 (unless (display-graphic-p)
-  (setq interprogram-cut-function
+    (xterm-mouse-mode 1)
+    (setq interprogram-cut-function
         (lambda (text &optional _)
-          (with-temp-buffer (insert text) (call-process-region (point-min) (point-max) "pbcopy"))))
-  (setq interprogram-paste-function
+            (with-temp-buffer (insert text) (call-process-region (point-min) (point-max) "pbcopy"))))
+    (setq interprogram-paste-function
         (lambda ()
-          (with-temp-buffer (call-process "pbpaste" nil t) (buffer-string)))))
+            (with-temp-buffer (call-process "pbpaste" nil t) (buffer-string)))))
 
 ;;; Package management
 
@@ -44,59 +47,59 @@
 ;;; Editing
 
 (use-package evil
-  :ensure t
-  :init
-  (setq evil-want-integration t
+    :ensure t
+    :init
+    (setq evil-want-integration t
         evil-undo-system 'undo-redo
         evil-want-C-u-scroll t
         evil-want-C-u-delete t
         evil-vsplit-window-right t)
-  :config
-  (evil-mode 1)
-  (dolist (mode '(xref--xref-buffer-mode Buffer-menu-mode dired-mode flymake-diagnostics-buffer-mode diff-mode))
-    (evil-set-initial-state mode 'emacs))
-  (define-key evil-normal-state-map ";" 'evil-ex)
-  (define-key evil-normal-state-map ":" 'evil-repeat-find-char)
-  (define-key evil-visual-state-map ";" 'evil-ex)
-  (define-key evil-visual-state-map ":" 'evil-repeat-find-char)
-  (define-key evil-insert-state-map (kbd "C-h") 'delete-backward-char))
+    :config
+    (evil-mode 1)
+    (dolist (mode '(xref--xref-buffer-mode Buffer-menu-mode dired-mode flymake-diagnostics-buffer-mode diff-mode))
+        (evil-set-initial-state mode 'emacs))
+    (define-key evil-normal-state-map ";" 'evil-ex)
+    (define-key evil-normal-state-map ":" 'evil-repeat-find-char)
+    (define-key evil-visual-state-map ";" 'evil-ex)
+    (define-key evil-visual-state-map ":" 'evil-repeat-find-char)
+    (define-key evil-insert-state-map (kbd "C-h") 'delete-backward-char))
 
 (use-package evil-surround
-  :ensure t
-  :init (global-evil-surround-mode 1))
+    :ensure t
+    :init (global-evil-surround-mode 1))
 
 (use-package company
-  :ensure t
-  :custom
-  (company-idle-delay 0.2)
-  (company-minimum-prefix-length 2)
-  (company-tooltip-limit 10)
-  :init (global-company-mode 1))
+    :ensure t
+    :custom
+    (company-idle-delay 0.2)
+    (company-minimum-prefix-length 2)
+    (company-tooltip-limit 10)
+    :init (global-company-mode 1))
 
 ;;; LSP
 (use-package eglot
-  :defer t
-  :custom
-  (eglot-autoshutdown t)
-  (eglot-send-changes-idle-time 0.5)
-  :config
-  (define-key eglot-mode-map (kbd "C-c C-a") 'eglot-code-actions)
-  (define-key eglot-mode-map (kbd "C-c f") #'eglot-format-buffer)
-  (define-key eglot-mode-map (kbd "C-c r") #'xref-find-references))
+    :defer t
+    :custom
+    (eglot-autoshutdown t)
+    (eglot-send-changes-idle-time 0.5)
+    :config
+    (define-key eglot-mode-map (kbd "C-c C-a") 'eglot-code-actions)
+    (define-key eglot-mode-map (kbd "C-c f") #'eglot-format-buffer)
+    (define-key eglot-mode-map (kbd "C-c r") #'xref-find-references))
 
 (setq eglot-server-programs
-      '((php-mode . ("phpactor" "language-server"))
-        (js-mode . ("vtsls" "--stdio"))
-        (tsx-mode . ("vtsls" "--stdio"))
-        (typescript-mode . ("vtsls" "--stdio"))
-        (css-mode . ("css-languageserver" "--stdio"))))
+    '((php-mode . ("phpactor" "language-server"))
+         (js-mode . ("vtsls" "--stdio"))
+         (tsx-mode . ("vtsls" "--stdio"))
+         (typescript-mode . ("vtsls" "--stdio"))
+         (css-mode . ("css-languageserver" "--stdio"))))
 
 (dolist (hook '(php-mode-hook js-mode-hook typescript-mode-hook tsx-mode-hook css-mode-hook lua-mode-hook))
-  (add-hook hook 'eglot-ensure))
+    (add-hook hook 'eglot-ensure))
 
 (setq flymake-fringe-indicator-position 'left-fringe
-      flymake-show-diagnostics-at-end-of-line nil
-      flymake-suppress-zero-counters t)
+    flymake-show-diagnostics-at-end-of-line nil
+    flymake-suppress-zero-counters t)
 
 ;;; Programming modes
 (use-package php-mode :ensure t :mode "\\.php\\'")
@@ -108,37 +111,40 @@
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . tsx-mode))
 
 (use-package markdown-mode
-  :ensure t
-  :mode ("\\.md\\'" . markdown-mode)
-  :init (setq markdown-command "pandoc"))
+    :ensure t
+    :mode ("\\.md\\'" . markdown-mode)
+    :init (setq markdown-command "pandoc"))
 
 ;;; Terminal
 (use-package eat
-  :ensure t
-  :commands (eat)
-  :config
-  (eat-eshell-mode 1)
-  (add-hook 'eat-mode-hook
-            (lambda ()
-              (evil-emacs-state)
-              (setq-local show-trailing-whitespace nil))))
+    :ensure t
+    :commands (eat)
+    :config
+    (eat-eshell-mode 1)
+    (add-hook 'eat-mode-hook
+        (lambda ()
+            (evil-emacs-state)
+            (setq-local show-trailing-whitespace nil))))
 
 ;;; Git
 (use-package magit
-  :ensure t
-  :commands (magit-status))
+    :ensure t
+    :commands (magit-status)
+    :config
+    (setq magit-diff-refine-hunk 'all
+        magit-log-arguments '("--no-graph" "-n256")))
 
 (use-package diff-hl
-  :ensure t
-  :config
-  (global-diff-hl-mode 1)
-  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh t))
+    :ensure t
+    :config
+    (global-diff-hl-mode 1)
+    (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh t))
 
 ;;; Org
 (use-package org
-  :defer t
-  :config
-  (setq org-use-property-inheritance nil
+    :defer t
+    :config
+    (setq org-use-property-inheritance nil
         org-log-done nil
         org-adapt-indentation nil
         org-deadline-warning-days 3
@@ -146,17 +152,17 @@
         org-todo-keywords '((sequence "TODO" "STARTED" "|" "DONE"))
         org-default-notes-file "~/org/tasks.org"
         org-agenda-files (append
-                          (directory-files "~/org/" t "\\.org$")
-                          (directory-files-recursively "~/vault-org/projects/" "\\.org$"))
+                             (directory-files "~/org/" t "\\.org$")
+                             (directory-files-recursively "~/vault-org/projects/" "\\.org$"))
         org-capture-templates
         '(("n" "Note" entry (file "~/org/inbox.org") "* %?\n  %u"))
         org-agenda-custom-commands
         '(("p" "Projects Agenda"
-           ((agenda "")
-            (tags-todo "+TODO=\"TODO\""))
-           ((org-agenda-files
-             (directory-files-recursively
-              "~/vault-org/projects/" "\\.org$")))))))
+              ((agenda "")
+                  (tags-todo "+TODO=\"TODO\""))
+              ((org-agenda-files
+                   (directory-files-recursively
+                       "~/vault-org/projects/" "\\.org$")))))))
 
 ;;; Keybindings
 (global-set-key (kbd "C-c t") #'eat)
@@ -170,4 +176,4 @@
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
-  (load custom-file))
+    (load custom-file))
