f06fc5b8bc573c05e5e50137274817e2a2ef69a5
Yeri <mail@yeripratama.com>
2026-05-28 14:22:34 +0700

    nvim: remove custom highlight

--- home/.config/nvim/plugin/highlight.lua
+++ home/.config/nvim/plugin/highlight.lua
@@ -4,8 +4,6 @@ local function set_hl()
 
   if vim.o.background == "dark" then
     vim.api.nvim_set_hl(0, "WinSeparator", { fg = "NvimDarkGrey3" })
-    vim.api.nvim_set_hl(0, "DiffAdd", { fg = "NONE", bg = "#1a3a2a" })
-    vim.api.nvim_set_hl(0, "@org.keyword.done", { fg = "#00FF00" })
     vim.api.nvim_set_hl(0, "StatusLine", { bg = "NvimDarkGrey1" })
     vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NvimDarkGrey1", fg = "NvimDarkGrey4" })
   elseif vim.o.background == "light" then
