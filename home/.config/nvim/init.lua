759617677fa7cf9a7d51ee4a85394497a6e8862d
Yeri <mail@yeripratama.com>
2026-06-08 02:13:03 +0700

    'matchit remap to m'

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -43,7 +43,8 @@ vim.keymap.set({ "n", "v" }, "gh", "0")
 vim.keymap.set({ "n" }, "gl", "$")
 vim.keymap.set({ "v" }, "gl", "$h")
 vim.keymap.set({ "n", "v" }, "ge", "G")
-vim.keymap.set({ "n", "x" }, "mm", "%")
+vim.keymap.set({ "n" }, "mm", "%")
+vim.keymap.set({ "x" }, "am", "a%", { remap = true })
 vim.keymap.set("n", "ga", "<cmd>b#<cr>")
 
 -- terminal
