1bf1d72820ccb5e51665707d3d30fed2d72f95fc
Yeri <mail@yeripratama.com>
2026-07-05 21:34:41 +0700

    winbar

---
+++ home/.config/nvim/plugin/winbar.lua
@@ -0,0 +1,15 @@
+vim.o.cmdheight = 0
+vim.o.laststatus = 0
+
+local exclude_buftypes = { 'quickfix', 'nofile', 'help', 'terminal', 'prompt' }
+
+vim.api.nvim_create_autocmd({ 'BufWinEnter', 'WinEnter' }, {
+  callback = function()
+    if vim.tbl_contains(exclude_buftypes, vim.bo.buftype) then
+      vim.wo.winbar = ''
+    else
+      vim.wo.winbar = "%= %t %m %="
+    end
+  end,
+})
+
