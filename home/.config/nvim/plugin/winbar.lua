357a01e4fb949758cf39ac1ed512104cdb85b095
Yeri <mail@yeripratama.com>
2026-07-05 21:43:52 +0700

    skip winbar for new empty buffer

--- home/.config/nvim/plugin/winbar.lua
+++ home/.config/nvim/plugin/winbar.lua
@@ -5,7 +5,7 @@ local exclude_buftypes = { 'quickfix', 'nofile', 'help', 'terminal', 'prompt' }
 
 vim.api.nvim_create_autocmd({ 'BufWinEnter', 'WinEnter' }, {
   callback = function()
-    if vim.tbl_contains(exclude_buftypes, vim.bo.buftype) then
+    if vim.tbl_contains(exclude_buftypes, vim.bo.buftype) or vim.api.nvim_buf_get_name(0) == '' then
       vim.wo.winbar = ''
     else
       vim.wo.winbar = "%= %t %m %="
