d086a0c12a518b7527b8a1f94394af4473aa9031
Yeri <mail@yeripratama.com>
2026-07-08 16:11:04 +0700

    move lualine to spec

--- home/.config/nvim/lua/specs.lua
+++ home/.config/nvim/lua/specs.lua
@@ -178,7 +178,144 @@ local lspconfig = {
 }
 local satellite = { src = "lewis6991/satellite.nvim" }
 local illuminate = { src = "RRethy/vim-illuminate" }
-local lualine = require("plugins.lualine")
+local lualine = {
+  src = "nvim-lualine/lualine.nvim",
+  config = function()
+    vim.o.showtabline = 0
+
+    local icons = require("icons")
+
+    local conditions = {
+      buffer_not_empty = function()
+        return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+      end,
+      buffer_empty = function()
+        return vim.fn.empty(vim.fn.expand("%:t")) == 1
+      end,
+      screen_width = function(min_w)
+        return function()
+          return vim.o.columns > min_w
+        end
+      end,
+      check_git_workspace = function()
+        local filepath = vim.fn.expand("%:p:h")
+        local gitdir = vim.fn.finddir(".git", filepath .. ";")
+        return gitdir and #gitdir > 0 and #gitdir < #filepath
+      end,
+      diff_mode = function()
+        return vim.o.diff == true
+      end,
+    }
+
+    local branch = {
+      "branch",
+      icon = icons.get("branch"),
+      color = { gui = "bold" },
+    }
+
+    local filename = {
+      "filename",
+      path = 1,
+    }
+
+    local lsp = {
+      "lsp_status",
+      cond = conditions.screen_width(120),
+    }
+
+    local diff = {
+      "diff",
+      cond = conditions.screen_width(120),
+    }
+
+    local diagnostics = {
+      "diagnostics",
+      sources = { "nvim_diagnostic" },
+      symbols = icons.diagnostics_symbols(),
+      cond = conditions.screen_width(120),
+    }
+
+    local cwd = {
+      function()
+        return icons.get("folder") .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+      end,
+      color = { gui = "bold" },
+    }
+
+    local macro = {
+      function()
+        local char_register = vim.fn.reg_recording()
+        if #char_register > 0 then
+          return "REC @" .. char_register
+        end
+        return ""
+      end,
+      color = "Cursor",
+    }
+
+    local selectioncount = {
+      "selectioncount",
+      color = "Cursor",
+    }
+
+    local location = {
+      "location",
+    }
+
+    local progress = {
+      "progress",
+    }
+
+    local tabs = {
+      "tabs",
+      tabs_color = {
+        active = "Cursor",
+        inactive = "StatusLine",
+      },
+      show_modified_status = false,
+      cond = function()
+        local tabcount = #vim.api.nvim_list_tabpages()
+        return tabcount > 1
+      end,
+    }
+    require("lualine").setup({
+      options = {
+        theme = {
+          normal = {
+            a = "StatusLine",
+            b = "StatusLine",
+            c = "StatusLine",
+          },
+        },
+        globalstatus = true,
+        always_divide_middle = false,
+        always_show_tabline = false,
+        component_separators = "",
+        section_separators = "",
+      },
+      sections = {
+        lualine_a = { tabs, cwd },
+        lualine_b = {},
+        lualine_c = {
+          filename,
+        },
+        lualine_x = {
+          lsp,
+          diagnostics,
+          selectioncount,
+          macro,
+        },
+        lualine_y = {
+          diff,
+          branch,
+          location,
+          progress,
+        },
+        lualine_z = {},
+      },
+    })
+  end,
+}
 local incline = {
   src = "b0o/incline.nvim",
   config = function()
